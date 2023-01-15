3bdcee4463b43f8147eda4a88ad476d1dd3a6b85
Muhammad Akbar Adityah <99787895+baloerrr@users.noreply.github.com>
2023-01-15 11:01:50 +0700

    Delete client directory

--- client/src/components/NewsLetter.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const NewsLetter = () => {
-  return (
-    <div>NewsLetter</div>
-  )
-}
-
-export default NewsLetter
--- client/src/pages/PorductList.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const PorductList = () => {
-  return (
-    <div>PorductList</div>
-  )
-}
-
-export default PorductList
