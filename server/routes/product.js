41f7ff1d062701fe12bd61d6bd200935072d18ad
Muhammad Akbar Adityah <99787895+baloerrr@users.noreply.github.com>
2023-01-15 11:00:32 +0700

    Update Product.js

--- server/routes/product.js
+++ server/routes/product.js
@@ -48,7 +48,7 @@ router.get("/find/:id", async(req,res) => {
     }
 });
 
-router.get("/" ,async(req,res)=> {
+router.get("/" , async(req,res)=> {
     const qNew = req.query.new;       
     const qCategory = req.query.category;       
   try {
